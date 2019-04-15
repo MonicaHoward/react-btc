dea738242cda119a034e7ecd40b9a4b8725e9e6d
Monica Howard <jay.m.howard@gmail.com>
2019-04-14 23:26:45 -0500

    Adds styling to Features section

--- src/Features.js
+++ src/Features.js
@@ -7,32 +7,34 @@ class Features extends React.Component {
         <div class="row mx-auto">
           <div class="card-group justify-content-center">
             <div class="card">
-              <img class="card-img-top" src="x" alt="Card image cap" />
-              <div class="card-body">
-                <h5 class="card-title">Reasonable Fees</h5>
-                <p class="card-text">
+              <img class="card-img-top " src="x" alt="Card image cap" />
+              <div class="card-body mx-5">
+                <h5 class="card-title mx-5 feature-title">REASONABLE FEES</h5>
+                <p class="card-text mx-5">
                   Stripe is an always-improving toolchain that gains new
                   features every month. Our world-class engineering team
                   constantly iterates upon every facet of the Stripe stack. And
                   from Apple Pay to Bitcoin, building on Stripe means you get
                   early access to the latest technologies.
                 </p>
-                <button type="button" class="btn btn-link">
+                <button type="button" class="btn btn-link mx-5 more-button">
                   Learn More
                 </button>
               </div>
             </div>
             <div class="card">
-              <img class="card-img-top" src="x" alt="Card image cap" />
-              <div class="card-body">
-                <h5 class="card-title">Early Verification</h5>
-                <p class="card-text">
+              <img class="card-img-top " src="x" alt="Card image cap" />
+              <div class="card-body mx-5">
+                <h5 class="card-title mx-5 feature-title">
+                  Early Verification
+                </h5>
+                <p class="card-text mx-5">
                   We help power 100,000+ businesses in 100+ countries and across
                   nearly every industry. Headquartered in San Francisco, Stripe
                   has 9 global offices and hundreds of people working to help
                   transform how modern businesses are built and run.
                 </p>
-                <button type="button" class="btn btn-link">
+                <button type="button" class="btn btn-link mx-5 more-button">
                   Learn More
                 </button>
               </div>
@@ -42,32 +44,34 @@ class Features extends React.Component {
         <div class="row mx-auto">
           <div class="card-group justify-content-center">
             <div class="card">
-              <img class="card-img-top" src="x" alt="Card image cap" />
-              <div class="card-body">
-                <h5 class="card-title">Reasonable Fees</h5>
-                <p class="card-text">
+              <img class="card-img-top " src="x" alt="Card image cap" />
+              <div class="card-body mx-5">
+                <h5 class="card-title mx-5 feature-title">
+                  QUICK TRANSACTIONS
+                </h5>
+                <p class="card-text mx-5">
                   Stripe is an always-improving toolchain that gains new
                   features every month. Our world-class engineering team
                   constantly iterates upon every facet of the Stripe stack. And
                   from Apple Pay to Bitcoin, building on Stripe means you get
                   early access to the latest technologies.
                 </p>
-                <button type="button" class="btn btn-link">
+                <button type="button" class="btn btn-link mx-5 more-button">
                   Learn More
                 </button>
               </div>
             </div>
             <div class="card">
               <img class="card-img-top" src="x" alt="Card image cap" />
-              <div class="card-body">
-                <h5 class="card-title">Early Verification</h5>
-                <p class="card-text">
+              <div class="card-body mx-5">
+                <h5 class="card-title mx-5 feature-title">INSTANT SUPPORT</h5>
+                <p class="card-text mx-5">
                   We help power 100,000+ businesses in 100+ countries and across
                   nearly every industry. Headquartered in San Francisco, Stripe
                   has 9 global offices and hundreds of people working to help
                   transform how modern businesses are built and run.
                 </p>
-                <button type="button" class="btn btn-link">
+                <button type="button" class="btn btn-link mx-5 more-button">
                   Learn More
                 </button>
               </div>
